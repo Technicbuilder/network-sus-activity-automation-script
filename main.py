5345232e74bc44408b66fcc8a05a92c64828ca74
Technicbuilder <151921436+Technicbuilder@users.noreply.github.com>
2026-01-16 09:32:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import time
 from network_scan import scan_network
 from database import store
-from tabulate import tabulate
 
-scan_result = scan_network()
+def main():
+    initial_scan_result = scan_network()
+    store(results=initial_scan_result)
+    # after storing intitial results, we must run another network scan 10 minutes
+    # then compare results to check for new ips or document any suspicious activity
+    while True:
+        time.sleep(600)
+        scan_result = scan_network()
 
-store(results=scan_result)
+
+if __name__ == "__main__":
+    main()
